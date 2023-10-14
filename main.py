be7904f32d43200ed27218129fbac496d13696be
Muammer Berber <muammerberber88@gmail.com>
2023-10-14 12:45:43 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import turtle
+from turtle import _Screen
+
+drawing_board: _Screen = turtle.Screen()
+drawing_board.bgcolor("sky blue")
+drawing_board.title("Python Turtle")
+
+turtle_instance = turtle.Turtle()
+
+def turtle_foward():
+    turtle_instance.forward(100)
+def rotate_angle_right():
+    turtle_instance.setheading(turtle_instance.heading()-10)
+def rotate_angle_left():
+    turtle_instance.setheading(turtle_instance.heading()+10)
+def clear_screen():
+    turtle_instance.clear()
+def turtle_return_home():
+     turtle_instance.home()
+def turtle_pen_up():
+    turtle_instance.penup()
+def turtle_pen_down():
+    turtle_instance.pendown()
+
+
+
+drawing_board.listen()
+drawing_board.onkey(fun=turtle_foward,key="space")
+drawing_board.onkey(fun=rotate_angle_right,key="Down")
+drawing_board.onkey(fun=rotate_angle_left,key="Up")
+drawing_board.onkey(fun=clear_screen,key="c")
+drawing_board.onkey(fun=turtle_return_home,key="h")
+drawing_board.onkey(fun=turtle_pen_down,key="w")
+drawing_board.onkey(fun=turtle_pen_up,key="q")
+
+turtle.mainloop()
